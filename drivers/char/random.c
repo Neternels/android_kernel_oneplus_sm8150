74f1a0c135923d1a04fc70f93c3d3a4af0b8ed98
Cyber Knight <cyberknight755@gmail.com>
2021-08-08 03:42:42 +0800

    drivers: fix mismerge
    Signed-off-by: Cyber Knight <cyberknight755@gmail.com>

--- drivers/char/random.c
+++ drivers/char/random.c
@@ -1028,42 +1028,6 @@ static void crng_backtrack_protect(__u8 tmp[CHACHA_BLOCK_SIZE], int used)
 	_crng_backtrack_protect(crng, tmp, used);
 }
 
-static ssize_t extract_crng_user(void __user *buf, size_t nbytes)
-{
-	ssize_t ret = 0, i = CHACHA_BLOCK_SIZE;
-	__u8 tmp[CHACHA_BLOCK_SIZE] __aligned(4);
-	int large_request = (nbytes > 256);
-
-	while (nbytes) {
-		if (large_request && need_resched()) {
-			if (signal_pending(current)) {
-				if (ret == 0)
-					ret = -ERESTARTSYS;
-				break;
-			}
-			schedule();
-		}
-
-		extract_crng(tmp);
-		i = min_t(int, nbytes, CHACHA_BLOCK_SIZE);
-		if (copy_to_user(buf, tmp, i)) {
-			ret = -EFAULT;
-			break;
-		}
-
-		nbytes -= i;
-		buf += i;
-		ret += i;
-	}
-	crng_backtrack_protect(tmp, i);
-
-	/* Wipe data just written to memory */
-	memzero_explicit(tmp, sizeof(tmp));
-
-	return ret;
-}
-
-
 /*********************************************************************
  *
  * Entropy input management
